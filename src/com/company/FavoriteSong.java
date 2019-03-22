f5a49f7b23aec9e72b65ecd1a85e9c25f4af8cd2
Ron Kang <ronjinhong.kang@my.jcu.edu.au>
2019-03-22 10:53:10 +0800

    Added FavoriteSong and MovieQuoteInfo Exercises

---
+++ src/com/company/FavoriteSong.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class FavoriteSong {
+    public static void main(String[] args) {
+        // Song Name
+        System.out.println("Song - Only my railgun");
+        // Song Line 1
+        System.out.println("Looking!");
+        // Song Line 2
+        System.out.println("The blitz loop this planet to search way.");
+        // Song Line 3
+        System.out.println("Only my railgun can shoot it  right this instance");
+        // Song Line 4
+        System.out.println("This definite sensation");
+    }
+}
---
+++ src/com/company/MovieQuoteInfo.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class MovieQuoteInfo {
+
+    public static void main(String[] args) {
+        // Movie Quote
+        System.out.println("Captain Jack Sparrow!");
+        // Movie Name
+        System.out.println("Pirates of the Carribien - The Curse of the black pearl");
+        // Movie Quoted character
+        System.out.println("Jack Sparrow");
+        // Movie Quoted year
+        System.out.println("2003");
+    }
+}
